16deb65eec00f44d160f2d7b59c59e8d7d4f0017
Peter A. Higgins <pahiggins@cenabo.com>
2019-02-09 11:08:08 +0000

    Update Main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+import firebase from 'react-native-firebase'
+
 export default class Main extends React.Component {
   state = { currentUser: null }
+  componentDidMount() {
+    const { currentUser } = firebase.auth()
+    this.setState({ currentUser })
+  }
   render() {
     const { currentUser } = this.state
     return (
