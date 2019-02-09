a17c932c3a4afd1f079a075fc592ee979eb40ad9
Peter A. Higgins <pahiggins@cenabo.com>
2019-02-09 11:08:59 +0000

    Update Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import firebase from 'react-native-firebase'
+
 export default class Login extends React.Component {
   state = { email: '', password: '', errorMessage: null }
   handleLogin = () => {
-    // TODO: Firebase stuff...
-    console.log('handleLogin')
+    const { email, pasword } = this.state
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => this.props.navigation.navigate('Main'))
+      .catch(error => this.setState({ errorMessage: error.message }))
   }
   render() {
     return (
