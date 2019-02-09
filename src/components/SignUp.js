8a89b4a6cad996af86e34584024ca04bd284ce5a
Peter A. Higgins <pahiggins@cenabo.com>
2019-02-09 11:06:48 +0000

    Update SignUp component

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import firebase from 'react-native-firebase'
+
 export default class SignUp extends React.Component {
   state = { email: '', password: '', errorMessage: null }
   handleSignUp = () => {
-    // TODO: Firebase stuff...
-    console.log('handleSignUp')
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then(() => this.props.navigation.navigate('Main'))
+      .catch(error => this.setState({ errorMessage: error.message }))
   }
   render() {
     return (
