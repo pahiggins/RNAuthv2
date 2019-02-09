70b301fd94f3f942f0d5b0205cc9d1bdc88aa1fd
Peter A. Higgins <pahiggins@cenabo.com>
2019-02-09 11:05:10 +0000

    Update Loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import firebase from 'react-native-firebase'
+
 export default class Loading extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.props.navigation.navigate(user ? 'Main' : 'SignUp')
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
